812d696302cc889247df679d9efe27b500c9ebdd
RunicPillow <86902739+RunicPillow@users.noreply.github.com>
2022-01-22 00:24:17 -0600

    Add AplicationDbContext

--- backend-tareas/backend-tareas/Startup.cs
+++ backend-tareas/backend-tareas/Startup.cs
@@ -1,7 +1,9 @@
+using backend_tareas.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,7 @@ namespace backend_tareas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddControllers();
         }
 
