240cbca3517c977b25b32d63e4959de8530ebfec
RunicPillow <86902739+RunicPillow@users.noreply.github.com>
2022-01-22 01:03:29 -0600

    Add TareaController

---
+++ backend-tareas/backend-tareas/Controllers/TareaController.cs
@@ -0,0 +1,39 @@
+﻿using backend_tareas.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : Controller
+    {
+        private readonly AplicationDbContext _context;
+
+        public TareaController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listTareas = await _context.Tareas.ToListAsync();
+                return Ok(listTareas);
+            }
+            catch (System.Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
